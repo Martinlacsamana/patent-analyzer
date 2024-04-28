48317ba14fff1160df767df906afd2089d7cdef4
Jamie Walton <jamiewalton@berkeley.edu>
2024-04-28 13:39:00 -0700

    Allow page navigation, style analysis

--- my-next-app/app/patent/page.tsx
+++ my-next-app/app/patent/page.tsx
@@ -10,12 +10,36 @@ export default function Patent() {
     return (
       
         <div>
-          <main className="mt-10 mx-14">
+          <main className="my-10 mx-14 flex gap-5">
             <div 
-            className="h-[230px] p-6 rounded-2xl border border-[#DAE5EA] bg-white" 
-            style={{ boxShadow: '0px 1px 10px 0px rgba(0, 0, 0, 0.10)' }}>
-              <p className="text-base overflow-ellipsis font-normal leading-9 pt-[24px] pb-[12px]">Microfluidic device for deformable beads enrichment and self-regulated ordering and encapsulation in droplets.pdf</p>
+            className="p-6 rounded-2xl border border-[#DAE5EA] bg-white w-3/5" 
+            style={{ boxShadow: '0px 1px 10px 0px rgba(0, 0, 0, 0.10)'}}>
+              <p className="text-base truncate overflow-ellipsis font-normal pt-[24px] pb-[12px] w-3/5">Microfluidic device for deformable beads enrichment and self-regulated ordering and encapsulation in droplets.pdf</p>
               <PatentReader file={patent}/>
+            </div>
+            <div 
+            className="p-10 rounded-2xl border border-[#DAE5EA] bg-[#F8F8F8]" 
+            style={{ boxShadow: '0px 1px 10px 0px rgba(0, 0, 0, 0.10)'}}>
+              <p className="text-base overflow-ellipsis font-normal pt-[16px] pb-[12px] text-custom-blue">Problem Statement</p>
+              <p className="text-sm font-normal pb-[12px]">The patent addresses the challenges of achieving robust ordered delivery of deformable beads in microfluidic devices, as well as the difficulty in achieving high percentage singlet encapsulation with a constant pressure source system.</p>
+              <div className="inline-flex items-center gap-[12px] flex-[0_0_auto] relative pt-2 pb-6">
+                {["microfluidics", "beads", "singlet encapsulation"].map((tag, index) => (
+                  <div key={index} className="border border-solid border-[#a0a0a1] h-[26px] flex-[0_0_auto] px-[16px] py-[4px] rounded-[8px] justify-center">
+                    <p className="text-[#a0a0a1] text-xs font-normal">{tag}</p>
+                  </div>
+                ))}
+              </div>
+              <p className="text-base overflow-ellipsis font-normal pt-[16px] pb-[12px] text-custom-blue">Solution</p>
+              <p className="text-sm font-normal pb-[12px]">The inventors developed a microfluidic device that can reliably achieve high percentage singlet encapsulation with a constant pressure source system. This was achieved through the development of a pinch and reservoir sequence system for beads concentrating and delivery frequency stabilizing, as well as the design of a long funnel to guide and align deformable beads into a single row, and a core design to achieve self-regulated beads in droplets encapsulation within a constant pressure system.</p>
+              <div className="inline-flex items-center gap-[12px] flex-[0_0_auto] relative pt-2 pb-6">
+                {["pinch and reservoir", "funnel"].map((tag, index) => (
+                    <div key={index} className="border border-solid border-[#a0a0a1] h-[26px] flex-[0_0_auto] px-[16px] py-[4px] rounded-[8px] justify-center">
+                      <p className="text-[#a0a0a1] text-xs font-normal">{tag}</p>
+                    </div>
+                  ))}
+              </div>
+              <p className="text-base overflow-ellipsis font-normal pt-[16px] pb-[12px] text-custom-blue">Summary</p>
+              <p className="text-sm font-normal pb-[12px]">This invention introduces a novel microfluidic device designed to improve the handling and processing of deformable beads in a controlled environment. The device features a series of inlets and microfluidic channels that facilitate the encapsulation of these beads into droplets, a method that is crucial for advancing laboratory assays such as single-cell analyses and other microreactor applications.</p>
             </div>
           </main>
         </div>
--- my-next-app/components/PatentReader.tsx
+++ my-next-app/components/PatentReader.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback, useState, SyntheticEvent } from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-
-import type { PDFDocumentProxy } from 'pdfjs-dist';
+import './components.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
@@ -16,7 +15,7 @@ const options = {
 
 const resizeObserverOptions = {};
 
-const maxWidth = 800;
+const maxWidth = 500;
 
 type PDFFile = string | File | null;
 
@@ -25,8 +24,8 @@ interface PatentReaderProps {
   }
 
 export default function PatentReader({file}:PatentReaderProps) {
-  //const [file, setFile] = useState<PDFFile>('./patent-example.pdf');
-  const [numPages, setNumPages] = useState<number>();
+  const [numPages, setNumPages] = useState<number>(0);
+  const [pageNumber, setPageNumber] = useState<number>(1);
   const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
   const [containerWidth, setContainerWidth] = useState<number>();
 
@@ -37,28 +36,48 @@ export default function PatentReader({file}:PatentReaderProps) {
       setContainerWidth(entry.contentRect.width);
     }
   }, []);
+  
 
-  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: {numPages: number}): void {
     setNumPages(nextNumPages);
+    setPageNumber(1);
+  }
+
+  function previousPage() {
+    if (pageNumber > 1) {
+      setPageNumber(prevPageNumber => prevPageNumber - 1);
+    }
+  }
+
+  function nextPage() {
+    if (pageNumber < numPages) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1);
+        console.log('next');
+      }
+  }
+
+  function changePage(event:any) {
+    setPageNumber(Number(event.target.value));
   }
 
   return (
     <div className="bg-[#F8F8F8] px-4 py-6 border border-[#DAE5EA] rounded-[16px]">
-    {/* <div className="Example__container__load">
-        <label htmlFor="file">Load from file:</label>{' '}
-        <input onChange={onFileChange} type="file" />
-    </div> */}
-    <div className="Example__container__document" ref={setContainerRef}>
-        <Document file={file} options={options}>
-        {Array.from(new Array(numPages), (el, index) => (
-            <Page
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+        <div className="flex gap-4 items-center justify-center pb-4">
+            <div className="cursor-pointer" onClick={previousPage}>{"<"}</div>
+            <input 
+                type="text"
+                className="text-sm w-10 border border-[#C1C1C1] rounded-[5px] text-center"
+                value={pageNumber}
+                onChange={changePage}
             />
-        ))}
-        </Document>
-    </div>
+            <p className="text-sm">{`/ ${numPages}`}</p>
+            <div className="cursor-pointer" onClick={nextPage}>{">"}</div>
+        </div>
+        <div className="bg-transparent" ref={setContainerRef}>
+            <Document file={file} options={options} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
+            </Document>
+        </div>
     </div>
   );
 }
