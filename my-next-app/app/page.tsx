2124412088ef3269f559aca6c450d4bd0b31d020
Jamie Walton <jamiewalton@berkeley.edu>
2024-04-27 19:28:47 -0700

    Update homepage formatting

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -1,42 +1,96 @@
+'use client';
 import Image from "next/image";
 import {Download, Link2} from 'lucide-react'
 import PatentCard from "@/components/PatentCard";
 
 export default function Home() {
+
+  var uploadedFile;
+
+  const handleUpload = (event: { target: { files: any; }; }) => {
+    const file = event.target.files;
+    console.log(file);
+    uploadedFile = file.name;
+  }
+
+  function dropHandler(ev: { preventDefault: () => void; dataTransfer: { items: any; files: any; }; }) {
+    console.log("File(s) dropped");
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        // If dropped items aren't files, reject them
+        if (item.kind === "file") {
+          const file = item.getAsFile();
+          console.log(`… file[${i}].name = ${file.name}`);
+        }
+      });
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        console.log(`… file[${i}].name = ${file.name}`);
+        uploadedFile = file.name;
+      });
+    }
+  }
+
+  function dragOverHandler(ev: { preventDefault: () => void; }) {
+    console.log("File(s) in drop zone");
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  }
+
   return (
     
     <div className="flex justify-center items-center pt-[24px]">
-      <main className="w-2/3">
+      <main className="w-3/4 mt-10">
         {/* Header text */}
-        <p className="text-2xl font-normal leading-9">Hi, <span className="text-custom-blue">researcher 102-6</span>. Welcome back to your AI patent workspace.</p>
-        <p className="text-base font-normal leading-6 pt-[6px]">Simply upload a patent document and let our AI analyze the data in seconds. Uncover valuable insights to drive your innovation work.</p>
+        <p className="text-xl font-normal leading-9">Hi, <span className="text-custom-blue">researcher 102-6</span>. Welcome back to your AI patent workspace.</p>
+        <p className="text-sm font-normal leading-6 pt-[6px]">Simply upload a patent document and let our AI analyze the data in seconds. Uncover valuable insights to drive your innovation work.</p>
 
         {/* `Read a new patent` section */}
-        <p className="text-2xl font-normal leading-9 pt-[24px] pb-[12px]">Read a new patent</p>
-        <div className="flex flex-col h-[230px] p-[92px_516px] items-center justify-center gap-2.5 shrink-0 rounded-lg border border-[#DAE5EA] bg-white" style={{ boxShadow: '0px 1px 10px 0px rgba(0, 0, 0, 0.10)' }}>
+        <p className="text-xl font-normal leading-9 pt-[24px] pb-[12px]">Read a new patent</p>
+        <div 
+          className="flex flex-col h-[230px] items-center justify-center gap-2.5 shrink-0 rounded-lg border border-[#DAE5EA] bg-white" 
+          style={{ boxShadow: '0px 1px 10px 0px rgba(0, 0, 0, 0.10)' }}
+          id="drop_zone" onDrop={dropHandler} onDragOver={dragOverHandler}
+          >
           <div className="flex justify-center items-center space-x-2 pb-1">
             
-            <div className="bg-custom-blue rounded-full p-1">
+            {/* <div className="bg-custom-blue rounded-full p-1">
               <Download size={18} color="#ffffff" />
-            </div>
+            </div> */}
             
-            <p className="text-xl font-normal whitespace-nowrap">Upload or drop a file</p>
-
-
+            <div>
+              <p className="text-base font-normal whitespace-nowrap">Upload or drop a file</p>
+              <form>
+                <input
+                    id='upload'
+                    className="text-sm cursor-pointer"
+                    type='file'
+                    onChange={handleUpload}
+                    value={uploadedFile}
+                />
+            </form>
+            </div>
           </div>
          
-          <p className="text-base font-normal whitespace-nowrap pb-6">Click to upload or drag a PDF or DOCX file here</p>
+          {/* <p className="text-sm font-normal whitespace-nowrap pb-6">Click to upload or drag a PDF or DOCX file here</p>
 
           <button className="w-36 flex justify-center items-center">
             <div className="w-full flex justify-center items-center space-x-2 bg-custom-blue rounded-lg pl-4 pr-4 p-1">
               <Link2 size={24} color="#ffffff" />
               <p className="text-white text-base font-normal"> From URL </p>
             </div>
-          </button>
+          </button> */}
         </div>
 
         {/* `Recent History` section */}
-        <p className="text-2xl font-normal leading-9 pt-[24px] pb-[12px]">Recent History</p>
+        <p className="text-xl font-normal leading-9 pt-[24px] pb-[12px]">Recent History</p>
         <div className="flex justify-center w-full space-x-4 pb-10">
           
           <PatentCard
